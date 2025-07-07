0eb2777e7e6bfa17820d0d926e495ee435b4f5bc
Uzair390-Del <uzair.zairi321@gmail.com>
2025-07-07 15:12:06 +0500

    update

--- hang_man.py
+++ hang_man.py
@@ -12,3 +12,25 @@ def load_words(filename):
     except FileNotFoundError:
         print("Word file not found! please the file is present or not!")
         return []
+# hangman game logic
+def play_hangman(words):
+    word= random.choice(words).lower()
+    guessed_word=["-"]*len(word)
+    # eg if python then "_ _ _ _ _ _"
+    attempts= 6
+    guessed_letters= set()
+    print("Welcome to the hangman!")
+    print("Guess the word by entering one letter at a time")
+    while attempts>0 and "_" in guessed_word:
+        print(f"\n Word:"," " .join(guessed_word))
+        print(f"Guesses Left:{attempts}")
+        print(f"Guesses letters: {', '.join(sorted(guessed_letters))}")
+        guess= input("Guess a letter : ").lower()
+        if len(guess) !=1 or not guess.isalpha():
+            print("Invalid Inputs. please enter a single alphabetical number!")
+            continue
+        if guess in guessed_letters:
+            print("You already guessed that latter!")
+        elif guess in word:
+            print("correct!")
+            
